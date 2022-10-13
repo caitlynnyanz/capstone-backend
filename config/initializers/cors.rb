727f15348e37a94421c7493273067fa97524232d
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-10-12 18:44:25 -0600

    fixing cors?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "example.com", "localhost:8100", "localhost:8080", "631cae075fa5955b61ff8e5f--bucolic-axolotl-88b2d5.netlify.app", 
-    "bucolic-axolotl-88b2d5.netlify.app"
+    "bucolic-axolotl-88b2d5.netlify.app", "main--bucolic-axolotl-88b2d5.netlify.app", "https://guarded-badlands-99137.herokuapp.com/users' from origin 'https://main--bucolic-axolotl-88b2d5.netlify.app"
 
     resource "*",
       headers: :any,
